dd156856f56fc7c041f21d87174357c940ff6066
supersonic <zll040519@outlook.com>
2024-11-14 13:53:10 +0800

    first impl of speed up syscall

--- kernel/proc.c
+++ kernel/proc.c
@@ -131,7 +131,14 @@ found:
     release(&p->lock);
     return 0;
   }
-
+  
+  // Allocate a usyscall page
+  if((p->usyscall = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -139,6 +146,9 @@ found:
     release(&p->lock);
     return 0;
   }
+
+  // initialize the usyscall
+  p->usyscall->pid = p->pid;
 
   // Set up new context to start executing at forkret,
   // which returns to user space.
@@ -157,7 +167,10 @@ freeproc(struct proc *p)
 {
   if(p->trapframe)
     kfree((void*)p->trapframe);
+  if(p->usyscall)
+    kfree((void *)p->usyscall);  
   p->trapframe = 0;
+  p->usyscall = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -202,6 +215,14 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+   // map a read-only page table at USYSCALL
+  if (mappages(pagetable, USYSCALL, PGSIZE, 
+                (uint64)(p->usyscall), PTE_R | PTE_U) < 0) {
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  } 
   return pagetable;
 }
 
@@ -212,6 +233,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
@@ -297,7 +319,7 @@ fork(void)
   np->sz = p->sz;
 
   // copy saved user registers.
-  *(np->trapframe) = *(p->trapframe);
+  *(np->trapframe) = *(p->trapframe);  
 
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
