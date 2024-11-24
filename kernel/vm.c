9175c597e01bc221c7a0e7f9e76ae0ebeb896dde
supersonic <zll040519@outlook.com>
2024-11-24 16:01:57 +0800

    modifed uvmcopy

--- kernel/vm.c
+++ kernel/vm.c
@@ -309,33 +309,43 @@ uvmfree(pagetable_t pagetable, uint64 sz)
 // physical memory.
 // returns 0 on success, -1 on failure.
 // frees any allocated pages on failure.
+// 
+// NOTE: In copy-on-write fork, we just map
+// the parent's physics pages into the child, 
+// instead of allocating new pages
 int
 uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 {
   pte_t *pte;
   uint64 pa, i;
   uint flags;
-  char *mem;
+//   char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
       panic("uvmcopy: pte should exist");
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
+    
+    // clear the PTE_W in both PTES
+    *pte &= (~PTE_W);
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
+    
+    // COW!
+    // if((mem = kalloc()) == 0)
+    //   goto err;
+    // memmove(mem, (char*)pa, PGSIZE);
+    if(mappages(new, i, PGSIZE, (uint64)pa, flags) != 0){
+    //   kfree(mem);
       goto err;
     }
   }
   return 0;
 
  err:
-  uvmunmap(new, 0, i / PGSIZE, 1);
+  // do not free memory, beacues we did't allocate
+  uvmunmap(new, 0, i / PGSIZE, 0);
   return -1;
 }
 
